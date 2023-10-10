41ed873e2e067d75004fea20e42451d198f67862
Gonzalo689 <gcontrerasd01@iesalbarregas.es>
2023-10-10 03:07:47 +0200

    .

--- Ficheros/FicherosCriptado/src/FicheroCifrado.java
+++ Ficheros/FicherosCriptado/src/FicheroCifrado.java
@@ -1,43 +1,50 @@
+import java.util.LinkedList;
 import java.util.Random;
 
-public class Cifrado {
+public class FicheroCifrado {
     public static int[] generarArrayAleatorio(int longitud, int min, int max) {
         Random rand = new Random(Long.MAX_VALUE);
         int[] numeros = new int[longitud];
-
-        for (int i = 0; i < longitud; i++) 
-            numeros[i] = rand.nextInt(max - min + 1) + min;
-
+        int a;
+        LinkedList<Integer> numerosFaltan = new LinkedList<>();
+        
+        for (int i = 0; i < longitud; i++) {
+            if(i < min || i > max)
+               numeros[i] = 0; 
+            else{
+                a = rand.nextInt(max - min + 1) + min;
+                if(!numerosFaltan.contains(a)){
+                    numerosFaltan.add(a);
+                    numeros[i] = a;
+                }else
+                    i--;
+            }
+        }
         return numeros;
-    }
+    }    
     public static void main(String[] args)  {
         int[] numerosAleatorios = generarArrayAleatorio(255, 32, 126); 
         
-        for (int i = 32; i < 126; i++) {
-            System.out.print(numerosAleatorios[i] + " ");
-        }
-        String a = "Hola muy buenas";
+        String a = "AHola muy buenaZ";
         String codificado = "";
         for (int i = 0; i < a.length(); i++) {
             int ascii = a.charAt(i);
             int asciiAleatorio = numerosAleatorios[ascii];
             codificado += (char) asciiAleatorio;
         }
-        System.out.println(codificado);
 
+        System.out.println(codificado);
+        
         String descodificado="";
         for (int i = 0; i < codificado.length(); i++) {
             int ascii = codificado.charAt(i);
-            for (int j = 32; j < 126 ; j++) {
+            for (int j = 0; j <= numerosAleatorios.length ; j++) {
                 if (numerosAleatorios[j] == ascii) {
                     descodificado += (char) j;
-                    System.out.print(j);
                     break;
                 }
             }
         }
         System.out.println(descodificado);
-        
     }
-
 }
